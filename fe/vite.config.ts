33269b363a2167cdc54ce787e61666ecd018f3ae
Duy Phúc <dinhlephucduy@gmail.com>
2025-05-07 08:16:15 +0700

    fix mix https-http

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       '/api': {
         target: 'http://3.24.218.139:8080',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
       },
     },
